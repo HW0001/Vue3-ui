e4cb7b9a473d8dc5bb21abc75eb7a40d94ec24b7
HW0001 <1600860624@qq.com>
2020-10-15 21:55:38 +0800

    dialog增加事件以及修样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,39 @@
 <template>
     <div> 
-    dialog
-    <GDialog />
+    <h3>dialog示例</h3>
+    <g-button theme="primary" @click="toggle">toggle</g-button>
+    <g-dialog :okfn="okfn" :cancel="cancel" v-model:visible="visible" type="top" isGlobalClose >
+        <template v-slot:title>
+            <div> 我是外面的div
+            </div>
+        </template>
+        <template v-slot:content>
+            <p> p我是外面的内容sx撒旦水水水水但是但是
+            </p> 
+        </template>
+    </g-dialog>
     </div>
 </template>
 <script lang=ts>
+import { ref } from 'vue'
 import GDialog from '../lib/GDialog.vue'
+import GButton from '../lib/GButton.vue'
 export default {
-    components:{GDialog}
+    components:{GDialog,GButton},
+    setup(){
+        const okfn = ()=>{ 
+            return false
+        }
+        const cancel = ()=>{
+             return true
+        }
+        const visible =ref(false)
+        const toggle = ()=>{
+            visible.value=!visible.value
+        }
+        return{
+            okfn,cancel,visible,toggle
+        }
+    }
 }
 </script>
--- src/lib/GDialog.vue
+++ src/lib/GDialog.vue
@@ -1,51 +1,128 @@
 <template>
-<teleport to="body">
-<div class="golu-diaglog-mask"></div>
-    <div class="golu-dialog">
-        <header class="golu-header">我是标题</header>
-        <main class="golu-main">我是内容</main>
-        <footer class="golu-footer">
-        <g-button theme='primary'>ok</g-button>
-        <g-button>cancel</g-button>
-        </footer>
+  <teleport to="body">
+    <div @click="maskclick" class="golu-diaglog-mask" v-if="visible"></div>
+    <div class="golu-dialog" v-if="visible" :class="type">
+      <header class="golu-dialog-header">
+        <slot name="title">Title</slot>
+        <span @click="close" class="golu-dialog-close"></span>
+      </header>
+      <main class="golu-dialog-main">
+        <slot name="content">我是内容</slot>
+      </main>
+      <footer class="golu-dialog-footer">
+        <g-button @click="ok" class="golu-dialog-button" theme="primary">确认</g-button>
+        <g-button @click="cancel">取消</g-button>
+      </footer>
     </div>
-    </teleport>
+  </teleport>
 </template>
 <script lang="ts">
-import GButton from './GButton.vue'
+import { ref } from "vue";
+import GButton from "./GButton.vue";
 export default {
-    components:{
-        GButton
+  props: {
+    visible: {
+        type:Boolean,
+        default:false
+    },
+    okfn: Function,
+    cancel: Function,
+    type:{
+        type:String,
+        default:"top"
+    },
+    isGlobalClose:{
+        type:Boolean,
+        default:true
     }
-}
+  },
+  setup(props, context) {
+      console.log(props.type)
+    const close = () => {
+      context.emit("update:visible", false);
+    };
+    const ok = () => {
+      props.okfn?.() && close();
+      //   props.okfn?.() && close()
+    };
+    const cancel = () => {
+      props.cancel?.() && close();
+    };
+    const maskclick=()=>{
+        props.isGlobalClose && close()
+    }
+    return {
+      close,
+      ok,
+      cancel,
+      maskclick
+    };
+  },
+  components: {
+    GButton,
+  },
+};
 </script>
 <style lang="scss">
-    .golu-diaglog-mask{
-        width: 100vw;
-        height: 100vh;
-        position: absolute;
-        top: 0px;
-        left: 0px;
-        z-index: 10;
-        background-color: rgba(0,0,0,.45);
+.golu-diaglog-mask {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, 0.45);
+}
+.golu-dialog {
+  position: fixed;
+  left: 50%;  
+  background-color: #fff;
+  z-index: 11;
+  min-width: 350px;
+  border-radius: 4px;
+  &.top{
+    top: 25%;
+    transform: translate(-50%);
+  }
+  &.center{
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
+  &.bottom{
+    top:75%; 
+    transform: translate(-50%, -100%);
+  }
+  .golu-dialog-header {
+    padding: 15px;
+    color: rgba(0, 0, 0, 0.85);
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 22px;
+    position: relative;
+    .golu-dialog-close {
+      display: inline-block;
+      text-align: center;
+      top: 15px;
+      right: 20px;
+      position: absolute;
     }
-    .golu-dialog{
-        position:fixed;
-        left: 50%;
-        top:25%;
-        transform: translate(-50%,-50%);
-        background-color: #fff;
-        z-index: 11;
-        min-width: 350px;
-        border-radius: 4px;;
-        .golu-header{
-            padding:15px;
-        }
-        .golu-main{
-            padding:10px 25px;
-        }
-        .golu-footer{
-            padding: 10px;
-        }
+    .golu-dialog-close::before {
+      content: "\2716";
+      cursor: pointer;
     }
+  }
+  .golu-dialog-main {
+    padding: 10px 24px;
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.65);
+    font-variant: tabular-nums;
+  }
+  .golu-dialog-footer {
+    padding: 10px 20px;
+    float: right;
+    .golu-dialog-button {
+      margin-right: 20px;
+    }
+  }
+}
 </style>
