4a07536ca254b2d95c3e0902041800e4e22eac4b
HW0001 <1600860624@qq.com>
2020-10-18 15:26:11 +0800

    完善页面

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,64 +1,112 @@
 <template>
-  <div class="topnav">
-    <span class="micon" @click="toggleVisidle">三</span>
-    <div class="logo"><h3><router-link to='/home'> vu3 轮子组件</router-link></h3></div>
-    <div class="funcs">
-      <span>菜单一</span>
-      <span>菜单二</span>
+<div class="topnav">
+    <span class="micon" @click="toggleVisidle" v-if="visibleMenu">
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-caidan"></use>
+        </svg></span>
+    <div class="logo">
+        <router-link to='/home'>
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-gulu"></use>
+            </svg>
+            <span>轮子组件</span>
+        </router-link>
     </div>
-  </div>
+    <div class="funcs">
+        <span>菜单一</span>
+        <span>菜单二</span>
+    </div>
+</div>
 </template>
 
 <script lang="ts">
-import { inject, Ref } from "vue";
+import {
+    computed,
+    inject,
+    Ref
+} from "vue";
+import router from '../router';
+import component from '*.vue';
 export default {
-  setup() {
-    const asidecis = inject<Ref<boolean>>("asidevis");
-    const toggleVisidle = () => {
-      asidecis.value = !asidecis.value;
-      console.log(asidecis);
-    };
-    return { toggleVisidle };
-  },
+    setup(props, context) {
+        const asidecis = inject < Ref < boolean >> ("asidevis");
+        const toggleVisidle = () => {
+            asidecis.value = !asidecis.value;
+        };
+        const visibleMenu = computed(() => router.currentRoute.value.matched.some(e => e.path === "/doc"))
+        return {
+            toggleVisidle,
+            visibleMenu
+        };
+    },
 };
 </script>
+
 <style lang="scss" scoped>
 .topnav {
-  background-color: aquamarine;
-  height: 60px;
-  width: 100%;  
-  position: relative;
-  .micon{
-      position: absolute;
-      top:20px;
-      left: 20px;
-      display: none;
-  }
-  > .logo {
-    position: absolute;
-    left: 30px;
-    top: 0px;
-  }
-  > .funcs {
-    height: 100%;
-    position: absolute;
-    right: 150px;
-    top: 0px;
-    display: flex;
-    align-items: center;
-  }
-  @media (max-width:500px) {
-      display: flex;
-      justify-content: center;
-      >.funcs{
-          display: none;
-      }
-      .micon{
-          display: inline-block;
-      }
-      .logo{
-          position: static;
-      }
-  }
+    height: 60px;
+    width: 100%;
+    position: relative;
+
+    .micon {
+        position: absolute;
+        top: 20px;
+        left: 20px;
+        display: none;
+        background-color: rgba(197, 175, 165, .3);
+
+        >svg {
+            width: 30px;
+        }
+    }
+
+    >.logo {
+        position: absolute;
+        left: 30px;
+        top: 10px;
+
+        a {
+            svg {
+                width: 32px;
+                height: 32px;
+                vertical-align: middle;
+            }
+
+            span {
+                vertical-align: middle;
+                color: #273849;
+                font-weight: 500;
+                font-size: 20px;
+                margin-left: 5px;
+            }
+        }
+
+    }
+
+    >.funcs {
+        height: 100%;
+        position: absolute;
+        right: 150px;
+        top: 0px;
+        display: flex;
+        align-items: center;
+    }
+
+    @media (max-width:500px) {
+        display: flex;
+        justify-content: center;
+
+        >.funcs {
+            display: none;
+        }
+
+        .micon {
+            display: inline-block;
+        }
+
+        .logo {
+            position: static;
+        }
+    }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,45 @@
 <template>
-<div>
+<div class="topnavcontenor">
     <top-nav />
     <div class="content">
         <img src="../assets/logo.png" />
-        <p>一些很厉害的轮子组件</p>
+        <p>一个基于vue3打造的自定义组件项目，</p>
         <div> <a>github</a>
             <router-link to='/doc'>开始</router-link>
+        </div>
+    </div>
+</div>
+<div class="extend">
+    <div>
+        <div>
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-Vue"></use>
+            </svg>
+            <span>基于Vue3compositionAPI</span>
+        </div>
+    </div>
+    <div>
+        <div>
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-typescript"></use>
+            </svg>
+            <span>使用typescript语法</span>
+        </div>
+    </div>
+    <div>
+        <div>
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-typescript"></use>
+            </svg>
+            <span>广告位招租</span>
+        </div>
+    </div>
+    <div>
+        <div>
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-typescript"></use>
+            </svg>
+            <span>广告位招租</span>
         </div>
     </div>
 </div>
@@ -16,32 +50,76 @@ import TopNav from "../components/TopNav.vue";
 export default {
     components: {
         TopNav,
-    },
-    setup() {},
+    }
 };
 </script>
 
 <style lang="scss" scoped>
-.content {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-direction: column;
-    padding-top: 30px;
-    ;
+.topnavcontenor {
+    background: linear-gradient(145deg, rgba(222, 251, 250, 1) 0%, rgba(185, 229, 232, 1) 100%);
+    padding-bottom: 100px;
+    clip-path: ellipse(100% 90% at 50% 0);
 
-    img {
-        width: 200px;
+    .content {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        flex-direction: column;
+        padding-top: 30px;
+
+        img {
+            width: 200px;
+        }
+
+        a {
+            display: inline-block;
+            border: 1px solid #42b983;
+            text-align: center;
+            margin-right: 20px;
+            border-radius: 15px;
+            color: #fff;
+            padding: 10px 30px;
+            background: rgb(79, 192, 141);
+        }
+    }
+}
+
+.extend {
+    display: flex;
+    flex-wrap: wrap;
+
+    >div {
+        width: 32%;
+        height: 150px;
+
+        >div {
+            width: 280px;
+            margin-left: auto;
+            margin-right: auto;
+
+            >svg {
+                width: 64px;
+                height: 64px;
+                vertical-align: middle;
+            }
+
+            >span {
+                vertical-align: middle;
+            }
+        }
     }
 
-    a {
-        display: inline-block;
-        border: 1px solid #42b983;
-        text-align: center;
-        margin-right: 20px;
-        border-radius: 15px;
-        color: #42b983;
-        padding: 10px 30px;
+    @media (max-width:800px) {
+        >div {
+            width: 50%;
+
+        }
+    }
+
+    @media (max-width:500px) {
+        >div {
+            width: 100%;
+        }
     }
 }
 </style>
