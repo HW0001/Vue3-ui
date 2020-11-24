1ecd13b74a541a23d2e9152ee7c2a9813bfe5d03
HW0001 <1600860624@qq.com>
2020-11-24 19:35:18 +0800

    增加导出box

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,6 @@ export {default as GDialog} from './GDialog.vue';
 export {default as GSwitch} from './GSwitch.vue';
 export {default as GTab} from './Gtab.vue';
 export {default as GTabs} from './GTabs.vue';
+export {default as GCheckBox} from './GCheckBox.vue';
+export {default as GCheckBoxGroup} from './GCheckBoxGroup.vue';
 export {openGDialog as openGDialog} from './openGDialog';
